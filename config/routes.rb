a32f0c41930803492b0bb4b2500743a447a0abb0
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-08-21 10:29:10 +0200

    Add welcome page with the slogan

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get :dashboard, to: "dashboard#index"
+
+  root "welcome#index"
 end
---
+++ spec/features/welcome_page_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+feature "Welcome page" do
+  scenario "everyone can see welcome page" do
+    visit root_path
+
+    expect(page).to have_content "Your Movie DataBase"
+    expect(page).to have_content "Keep your library up to date"
+  end
+end
