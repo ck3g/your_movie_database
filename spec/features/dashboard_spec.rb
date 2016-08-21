0c12699f15445c995e789fa94c66add84c43f7f1
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-08-21 12:03:04 +0200

    Empty dashboard layout

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 feature "Dashboard" do
-  scenario "user can navigate to dashboard" do
-    create :user_example_com
-    sign_in_as "user@example.com"
+  context "when there is no movies yet" do
+    scenario "user can navigate to dashboard" do
+      create :user_example_com
+      sign_in_as "user@example.com"
 
-    visit root_path
-    click_link "Go to Dashboard"
+      visit root_path
+      click_link "Go to Dashboard"
 
-    expect(current_path).to eq dashboard_path
-    expect(page).to have_content "Dashboard"
+      expect(current_path).to eq dashboard_path
+      expect(page).to have_content "You have no Movies yet"
+      expect(page).to have_selector "a", text: "Add them Now!"
+    end
   end
 end
