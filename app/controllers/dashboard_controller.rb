ceafa2314569915ac7e84e3f31d7c094dae686d5
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-08-21 12:42:27 +0200

    Display recently added movies on Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
 
-  def index
+  RECENT_MOVIES_COUNT = 8
 
+  def index
+    @movies = current_user.movies.last(RECENT_MOVIES_COUNT)
   end
 end
--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
 feature "Dashboard" do
+  given!(:user) { create :user_example_com }
+
+  background do
+    sign_in_as "user@example.com"
+  end
+
   context "when there is no movies yet" do
     scenario "user can navigate to dashboard" do
-      create :user_example_com
-      sign_in_as "user@example.com"
-
       visit root_path
       click_link "Go to Dashboard"
 
       expect(current_path).to eq dashboard_path
       expect(page).to have_content "You have no Movies yet"
       expect(page).to have_selector "a", text: "Add them Now!"
+      expect(page).not_to have_selector "h2", text: "Recently added"
+    end
+  end
+
+  context "when there is user's movies" do
+    given!(:movie) { create :movie, user: user, title: "Ocean Eleven" }
+
+    scenario "user can see movies titles" do
+      visit root_path
+      click_link "Go to Dashboard"
+
+      expect(page).not_to have_content "You have no Movies yet"
+      expect(page).not_to have_selector "a", text: "Add them Now!"
+      expect(page).to have_selector "h2", text: "Recently added"
+      within "#movie_#{movie.id}" do
+        expect(page).to have_content "Ocean Eleven"
+      end
     end
   end
 end
